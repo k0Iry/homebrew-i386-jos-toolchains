d62f2dd3dfe62b820d2bd7a7e6a1b00d27c9dcb4
Iry <kljsandjb@me.com>
2020-10-07 13:30:39 +0200

    Update i386-jos-qemu.rb

--- i386-jos-qemu.rb
+++ i386-jos-qemu.rb
@@ -1,4 +1,4 @@
-class I386JosElfGdb < Formula
+class I386JosQemu < Formula
   homepage "http://pdos.csail.mit.edu/6.828/2018/tools.html"
   url "https://github.com/k0Iry/6.828-qemu/archive/2.3.0.tar.gz"
   sha256 "988f8df2cfddb806008a23604aea643885c10af515b43557cfc75f5911d80942"
